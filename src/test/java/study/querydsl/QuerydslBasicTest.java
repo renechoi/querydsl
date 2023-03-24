0a57ff8d4058b1c3d34464fb61f3433c3343bcb7
renechoi <renechoi90@gmail.com>
2023-03-24 17:04:02 +0900

    Initial commit

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,93 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static study.querydsl.entity.QMember.*;
+
+
+public class QuerydslBasicTest {
+
+
+    @PersistenceContext
+    EntityManager em;
+
+    JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
+
+    @BeforeEach
+    public void before() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL() {
+        //member1을 찾아라.
+        String qlString =
+        "select m from Member m " +
+                "where m.username = :username";
+        Member findMember = em.createQuery(qlString, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl() {
+        //member1을 찾아라.
+
+        QMember m = new QMember("m");
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))//파라미터 바인딩 처리
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void search() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10)))
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+
+    /**
+     * member.username.eq("member1") // username = 'member1'
+     *     member.username.ne("member1") //username != 'member1'
+     *     member.username.eq("member1").not() // username != 'member1'
+     * member.username.isNotNull() //이름이 is not null
+     *     member.age.in(10, 20) // age in (10,20)
+     *     member.age.notIn(10, 20) // age not in (10, 20)
+     *     member.age.between(10,30) //between 10, 30
+     *      member.age.goe(30) // age >= 30
+     *    member.age.gt(30) // age > 30
+     *   member.age.loe(30) // age <= 30
+     *   member.age.lt(30) // age < 30
+     * member.username.like("member%") //like 검색 member.username.contains("member") // like ‘%member%’ 검색 member.username.startsWith("member") //like ‘member%’ 검색
+     */
+}
