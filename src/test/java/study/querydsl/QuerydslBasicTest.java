24e1a68f4846c0308422c54177840015abc72c82
renechoi <renechoi90@gmail.com>
2023-03-24 17:51:29 +0900

    feat: querydsl case example

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions.*;
@@ -363,6 +364,23 @@ public class QuerydslBasicTest {
 
     }
 
+
+    @Test
+    void basicCase() {
+        List<String> result = queryFactory
+                .select(member.age
+                        .when(10).then("열살").when(20).then("스무살").otherwise("기타"))
+                .from(member)
+                .fetch();
+    }
+
+    @Test
+    void complexCase() {
+        List<String> result = queryFactory
+                .select(new CaseBuilder()
+                        .when(member.age.between(0, 20)).then("0~20살").when(member.age.between(21, 30)).then("21~30살").otherwise("기타"))
+                .from(member).fetch();
+    }
 
 
 }
